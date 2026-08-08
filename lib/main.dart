d286bb3ea7a79b5dad720fdb18c654cbc739e267
yakup2525 <yakup_okumus@outlook.com>
2026-08-08 20:12:40 +0300

    Update dependencies: add marionette_flutter and logging packages; initialize Marionette in debug mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:marionette_flutter/marionette_flutter.dart';
 import 'package:pacman/feature/game/cubit/_cubit.dart';
 
 import 'feature/feature.dart';
 
 void main() {
+  // Initialize Marionette only in debug mode
+  if (kDebugMode) {
+    MarionetteBinding.ensureInitialized();
+  } else {
+    WidgetsFlutterBinding.ensureInitialized();
+  }
+
   runApp(const MyApp());
 }
 
